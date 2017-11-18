682cf435064069d90868fa428a2a3f224c5bd87d
rezabagusp <rezabaguspermana.rbp@gmail.com>
2017-11-18 11:28:09 +0700

    cleaning file

--- api/models/tingkat.model.js
+++
@@ -1,6 +0,0 @@
-var sequelize = require('./../dbsequelize');
-module.exports = function(sequelize, DataType){
-	return sequelize.define('tingkat',{
-        nama_tingkat: DataType.STRING
-    });
-}

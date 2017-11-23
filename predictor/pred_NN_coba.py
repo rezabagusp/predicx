e7c8953092728d90cd5ca79779ade7339adef8ea
Fadhlal Khaliq Surado <fksutan.rajomudo@gmail.com>
2017-11-23 15:04:51 +0700

    Script lebih dinamis

--- predictor/pred_NN_coba.py
+++ predictor/pred_NN_coba.py
@@ -1,10 +1,70 @@
-import pickle, json
+import pickle, json, sys, getopt
 from sklearn.preprocessing import StandardScaler
 from sklearn.externals import joblib 
 
-mlp = joblib.load('model/smt3_mat219_test.pkl')
-scaler = joblib.load('model/smt3_mat219_test_scaler.pkl')
+model_name = ""
+data = []
 
-x = mlp.predict(scaler.transform([[6,6,15]]))
-print(x)
-# print(json.dumps(x))
+def main(argv):
+	usage = "Usage:\
+					 \ntest.py -m <model_name> -d <data>\n \
+					 \nEx: test.py -m \"model_name\" -d \"5,5,6\" \
+					 \nModel name is not requiring extension (.pkl). Data (d) size must be respect to model\n\
+					 \n[Optional] \
+					 \n-h --help\tShow usage \
+					"
+
+	if(not len(argv)):
+		print(usage)
+		sys.exit(2)
+
+	try:
+		opts, args = getopt.getopt(argv,"hm:d:",["help=", "model=", "data="])
+	except getopt.GetoptError:
+		print(usage)
+		sys.exit(2)
+
+	for (opt,arg) in opts:
+		if opt in ('-h', "--help"):
+			print(usage)
+			sys.exit()
+		elif opt in ("-m", "--model"):
+			global model_name
+			model_name = arg
+		elif opt in ("-d", "--data"):
+			global data
+			data = arg.split(',')
+
+	if data == "" or model_name == "":
+		print(usage)
+		sys.exit()
+
+
+if __name__ == '__main__':
+	main(sys.argv[1:])
+
+	try:
+		mlp = joblib.load('model/'+model_name+'.pkl')
+		scaler = joblib.load('model/'+model_name+'_scaler.pkl')
+	except FileNotFoundError:
+		print('No such model name!')
+		sys.exit()
+
+	x = mlp.predict(scaler.transform([data]))[0]
+	# print(x)
+
+	if(x == 6):
+		print('A')
+	elif(x == 5):
+		print('AB')
+	elif(x == 4):
+		print('B')
+	elif(x == 3):
+		print('BC')
+	elif(x == 2):
+		print('C')
+	elif(x == 1):
+		print('D')
+	else:
+		print('E')
+	# print(json.dumps(x))
--- predictor/pred_NNtraining_coba.py
+++ predictor/pred_NNtraining_coba.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-import pickle
+import pickle, sys, getopt, time
 
 from sklearn.utils import shuffle
 from sklearn.neural_network import MLPClassifier
@@ -8,6 +8,130 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import classification_report,confusion_matrix
 
+
+start_time = time.clock()
+csv_name = ""
+model_name = ""
+class_name = ""
+is_testing = False
+
+def clean_file(data):
+	#change 'object' type data to categorical (ordinal) data
+	ordered_score = ['E', 'D', 'C', 'BC', 'B', 'AB', 'A']
+
+	attributes = list(data.keys())
+
+	for att in attributes:
+		if data[att].dtype == np.object:
+			data[att] = data[att].astype('category', ordered=True, categories=ordered_score).cat.codes
+	del data['id']
+
+	return data
+
+def splitting(data, test_size, random_state):
+	#randomize data
+	data = shuffle(data)
+
+	#separate data (X) and class (y)
+	X = data.loc[:,data.columns != class_name]
+	y = data.loc[:,data.columns == class_name]
+	y = np.ravel(y) # convert DataFrame to array
+
+	return train_test_split(X,y,test_size=test_size, random_state=random_state)
+
+def preprocessing(train, test):
+	#preprocessing
+	scaler = StandardScaler()
+	scaler.fit(train)
+	train_new = scaler.transform(train)
+	test_new = scaler.transform(test)
+
+	with open('model/'+model_name+'_scaler.pkl', 'wb') as scale:
+		pickle.dump(scaler, scale)
+
+	return train_new, test_new
+
+
+def training(X_train, y_train, X_test=None, y_test=None):
+	#train data
+	mlp = MLPClassifier(hidden_layer_sizes=(30,60,20))
+	mlp.fit(X_train, y_train)
+
+	if(is_testing):
+		if(X_test is not None and y_test is not None):
+			#testing (for testing only)
+			predictions = mlp.predict(X_test)
+			print(confusion_matrix(y_test,predictions))
+			print(mlp.score(X_test,y_test))
+			print(classification_report(y_test,predictions))
+			print(predictions)
+		else:
+			print("No testing data!")
+	else:
+		with open('model/'+model_name+'.pkl', 'wb') as model:
+			pickle.dump(mlp, model)
+
+def main(argv):
+	usage = "Usage:\
+					 \ntest.py -i <csv_name> -c <class_name>\n \
+					 \n[Optional] \
+					 \n-h --help\tShow usage \
+					 \n-t --testing\tDo and show testing result\n \
+					"
+
+	if(not len(argv)):
+		print(usage)
+		sys.exit(2)
+
+	try:
+		opts, args = getopt.getopt(argv,"thi:c:",["testing=", "help=", "input=","class="])
+	except getopt.GetoptError:
+		print(usage)
+		sys.exit(2)
+
+	for (opt,arg) in opts:
+		if opt in ('-h', "--help"):
+			print(usage)
+			sys.exit()
+		elif opt in ("-t", "--testing"):
+			global is_testing
+			is_testing = True
+		elif opt in ("-c", "--class"):
+			global class_name
+			class_name = arg
+		elif opt in ("-i", "--input"):
+			global csv_name
+			csv_name = arg
+
+	global model_name
+	model_name = csv_name[:-4]
+	
+	if csv_name == "" or class_name == "":
+		print(usage)
+		sys.exit()
+
+if __name__ == '__main__':
+	main(sys.argv[1:])
+
+	try:
+		nilai = pd.read_csv('data/'+csv_name)
+	except FileNotFoundError:
+		print("No such file!")
+		sys.exit(2)
+
+	nilai = clean_file(nilai)
+
+	X_train,X_test,y_train,y_test = splitting(nilai,0.10,32)
+	X_train,X_test = preprocessing(X_train,X_test)
+
+	if(is_testing):
+		training(X_train,y_train,X_test,y_test)
+	else:
+		training(X_train,y_train)
+	print("Done in %s seconds" % (time.clock()-start_time))
+
+
+'''
 #read file
 nilai = pd.read_csv('data/smt3_mat219_test.csv')
 #change 'object' type data to categorical (ordinal) data
@@ -59,5 +183,5 @@ try:
 except:
 	print('Failed to create model, please try again.')
 
-
+'''
 
